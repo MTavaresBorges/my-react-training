fe13324d455a72afaf0795716a2703e89c121837
MTavaresBorges <matheustavaresborges2000@gmail.com>
2024-08-24 15:35:52 -0300

    application commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Tweet } from "./components/Tweet.tsx";
+import { AppRoutes } from "./Routes.tsx";
+
+import './App.css';
+
+function App() {
+  return (
+    <AppRoutes />
+  );
+}
+
+export default App
